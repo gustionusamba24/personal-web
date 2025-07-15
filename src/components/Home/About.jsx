1e7ca305bd660613c4dec286b841491427db8f1f
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-07-15 13:09:01 +0700

    feat(home): finish the about section

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -1,5 +1,42 @@
 const About = () => {
-  return <div className="pt-28 lg:pt-36">About Page</div>;
+  return (
+    <section className="pt-20 lg:pt-32">
+      <div className="container">
+        <h1 className="heading-1">
+          About <span className="heading-2">Me</span>
+        </h1>
+
+        <div className="mt-4 items-center md:mt-6 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-10">
+          <div className="flex flex-col items-center justify-center gap-5 lg:gap-7">
+            <p className="about-description">
+              Gustio Nusamba is a software engineer with 1 year of experience.
+              His experience began when he joined at Ruang Guru as a frontend
+              engineer student and at Hacktiv8 as a backend engineer student.
+            </p>
+            <p className="about-description">
+              Proficient in both front end and back end, he brings a strong
+              foundation in Java, JavaScript, Typescript, and PHP in every
+              project. Currently, his goal is to develop his skills by taking
+              part in developing projects together with people who are
+              experienced in their fields.
+            </p>
+          </div>
+
+          <div className="hidden lg:block">
+            <figure className="ml-auto w-full max-w-[480px]">
+              <img
+                src="/images/about/gustio.jpg"
+                alt="Gustio Nusamba"
+                width={600}
+                height={800}
+                className="w-full rounded-[10px]"
+              />
+            </figure>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
