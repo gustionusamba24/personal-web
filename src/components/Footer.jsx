9a1ae6230e7e1cab0d5ab25688d31e343f9a0986
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-07-15 19:36:40 +0700

    feat(home): update logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -101,7 +101,7 @@ const Footer = () => {
 
         <div className="mb-8 flex items-center justify-between pt-10">
           <Link to="/" className="text-lightGreen">
-            Gustio Nusamba
+            <img src="/images/logo.svg" alt="Logo" width={36} height={36} />
           </Link>
 
           <p className="font-heading text-sm font-medium text-zinc-500">
