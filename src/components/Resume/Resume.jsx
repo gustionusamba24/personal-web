f52c76d1716d99688333aa0c4f3b0bb53286290b
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-07-16 15:28:56 +0700

    update resume page

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -2,7 +2,7 @@ const Resume = () => {
   return (
     <div className="h-screen">
       <h1 className="mt-10 text-center text-2xl font-bold text-zinc-50/80">
-        Resume is still work in progress
+        work in progress...
       </h1>
     </div>
   );
