da390041a90ce419d0039ffeaf2e33dbcd2bf184
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-07-17 13:48:24 +0700

    style(home): update minor styling on home page

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -4,16 +4,16 @@ const Hero = () => {
   return (
     <section className="section">
       <div className="container">
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-heading text-xl font-bold uppercase leading-tight tracking-tight text-zinc-50/50 lg:text-2xl">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <p className="text-center font-heading text-xl font-bold uppercase leading-tight tracking-tight text-zinc-50/50 lg:text-2xl">
             Full Stack Developer
           </p>
 
-          <h1 className="text-5xl font-semibold leading-tight tracking-normal text-zinc-50 md:text-[55px] lg:text-7xl lg:font-bold">
+          <h1 className="text-center text-5xl font-semibold leading-tight tracking-normal text-zinc-50 md:text-[55px] lg:text-7xl lg:font-bold">
             Gustio Nusamba
           </h1>
 
-          <div className="mt-5 flex gap-4 md:mt-8 lg:gap-6">
+          <div className="mt-5 flex w-full flex-col items-center justify-center gap-4 sm:flex-row md:mt-8 md:flex-row lg:gap-6">
             <Button
               label={"Download CV"}
               href={
